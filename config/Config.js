7e400ce04e4ea8e6b1b927e16e7676057475ea35
Kevin Toledo <kgtoledoc@unal.edu.co>
2018-10-06 07:06:20 -0500

    First commit

---
+++ config/Config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  DB: 'mongodb://localhost/movie_rating_app',
+  SECRET: 'movieratingappsecretkey',
+  FACEBOOK_APP_ID: '2429231850427853',
+  FACEBOOK_APP_SECRET: '25b21dfa2404778f2b7d656c4391ff50',
+}
